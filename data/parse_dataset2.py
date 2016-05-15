dfd13b74626b5ebc2867c7054ac7df6021e3e12e
mr-flannery <dennis_hempfing@gmx.de>
2016-05-15 12:51:02 +0200

    Implement xml to csv parser.

---
+++ data/parse_dataset2.py
@@ -0,0 +1,40 @@
+import xml.etree.ElementTree as ET
+import codecs
+
+fileName = "cddb_ID_nested_10000.xml"
+csvLines = []
+
+indexLookup = {
+	'did' : 0,
+	'artist' : 1,
+	'dtitle' : 2,
+	'category' : 3,
+	'genre' : 4,
+	'year' : 5,
+	'cdextra' : 6,
+}
+
+xmlTree = ET.parse(fileName)
+root = xmlTree.getroot()
+for disc in root:
+	values = ["","","","","","","",""]
+	# values = []
+	for child in disc:
+		if child.tag == 'tracks':
+			titles = []
+			for title in child:
+				titles.append(title.text)
+			titleString = "|".join(titles)
+			values[7] = titleString
+		elif child.text is not None:
+			values[indexLookup[child.tag]] += child.text
+		
+	csvLine = ",".join(values)
+	csvLines.append(csvLine)
+
+headline = "did,artist,dtitle,category,genre,year,cdextra,tracks"
+
+with codecs.open("cd_dataset.csv", 'w', 'utf-8') as csvFile:
+	csvFile.write(headline + "\n")
+	for line in csvLines:
+		csvFile.write(line + "\n")
