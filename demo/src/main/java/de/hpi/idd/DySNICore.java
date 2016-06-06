74ddb371b10aa8a9455bda39c772f97f697a221d
Philipp Schirmer <philipp@schirmer-online.de>
2016-06-06 16:49:02 +0200

    added stubs for the interface we should use

---
+++ demo/src/main/java/de/hpi/idd/DySNICore.java
@@ -0,0 +1,38 @@
+package de.hpi.idd;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DySNICore implements Core {
+
+	@Override
+	public void buildIndex(HashMap<String, String> parameters) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean destroyIndex(HashMap<String, String> parameters) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<String> getDuplicates(HashMap<String, String> record, HashMap<String, String> parameters) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<String> getRecord(String recordID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertRecord(HashMap<String, String> record, HashMap<String, String> parameters) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ demo/src/main/java/de/hpi/idd/DySNIExecuteInstructions.java
@@ -0,0 +1,21 @@
+package de.hpi.idd;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DySNIExecuteInstructions implements ExecuteInstructions {
+
+	@Override
+	public void executeFile(File fileIn, File fileOut, HashMap<String, String> parameters) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void executeInstruction(ArrayList<String> instruction) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
