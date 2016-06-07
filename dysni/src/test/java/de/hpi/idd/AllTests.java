fe6133767e3403881a21a5575092febf5cc9e478
Philipp Schirmer <philipp@schirmer-online.de>
2016-06-07 12:47:21 +0200

    merge base and dysni, use local dependencies for commons

---
+++ dysni/src/test/java/de/hpi/idd/AllTests.java
@@ -0,0 +1,15 @@
+package de.hpi.idd;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import de.hpi.idd.dysni.DySNIndexTest;
+import de.hpi.idd.dysni.avl.AVLTreeTest;
+import de.hpi.idd.util.SymmetricTableTest;
+import de.hpi.idd.util.UnionFindTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AVLTreeTest.class, DySNIndexTest.class, SymmetricTableTest.class, UnionFindTest.class })
+public class AllTests {
+}
